37509c6a29fa798bb9597a572a61500c9a9f52f0
rkrishnan42 <rohithk98@gmail.com>
2019-11-13 00:12:13 -0500

    added code to read from clock module and write to serial display

--- pet_feeder_main/pet_feeder_main.ino
+++ pet_feeder_main/pet_feeder_main.ino
@@ -1,18 +1,60 @@
+#include <Wire.h>
+#include <SparkFunDS1307RTC.h>
+
+#include <HX711.h>
 #include <Stepper.h>
-const int motor1 = 11;
-const int motor2 = 10;
-const int motor3 = 9;
-const int motor4 = 8;
+#include <LiquidCrystal.h>
+
+const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
+const int scale_dout = 3;
+const int scale_clk = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const int motor1 = 7;
+const int motor2 = 6;
+const int motor3 = 5;
+const int motor4 = 4;
+
+
+const int setWeightButton = 15;
+const int setTimeButton = 16;
+const int rtc_sqw = 14;
+
 
 Stepper motor = Stepper(4, motor1, motor2, motor3, motor4);
 
+int weightVal = 0;
 
 void setup() {
-
-  motor.setSpeed(5000);
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print("Current Time: ");
+  //pinMode(weightButton, INPUT_PULLUP);
+  int hour = 0;
+  int minute = 5;
+  int second = 10;
+  int day = 4;
+  int date = 13;
+  int month = 11;
+  int year = 19;
+  rtc.setTime(second, minute, hour, day, date, month, year);
+  //rtc.autoTime();
+  //motor.setSpeed(5000);
 }
 
 void loop() {
-  motor.step(1);
-  //delay(1000);
+  rtc.update();
+  int sec = rtc.second();
+  int m = rtc.minute();
+  int h = rtc.hour();
+  lcd.setCursor(1, 1);
+  lcd.print(String(h) + ":" + String(m) + ":" + String(sec));
+//  int buttonVal = digitalRead(weightButton);
+//  delay(1000);
+//  if(buttonVal == LOW) {
+//    weightVal += 1;
+//  }
+//  lcd.print(weightVal);
+  //motor.step(1);
+  
 }
